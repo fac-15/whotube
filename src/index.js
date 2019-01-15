8f6d8968aa86201dca691175878b4717ba192026
aterna01 <ssaniok90@mail.ru>
2019-01-15 12:05:17 +0000

    youtube API 4/4

--- src/index.js
+++ src/index.js
@@ -10,44 +10,17 @@ const app = express();
 app.use(morgan("tiny"));
 app.use(cors());
 
-// let cache;
-//Original URL
-// const url =
-//   "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UULNgu_OupwoeESgtab33CCw&maxResults=50";
+//hardcoded youtube username
 const youtuber = "PewDiePie";
+//API 1
 const userNameUrl =
   "https://www.googleapis.com/youtube/v3/channels?part=id%2Csnippet%2Cstatistics%2CcontentDetails%2CtopicDetails&forUsername=";
+//API 2
 const channelUrl =
   "https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=";
-// const getVideos = pageToken =>
-//   fetch(`${url}&key=${process.env.GOOGLE_API_KEY}`).then(response =>
-//     response.json()
-//   );
-
-// app.get("/videos", async (req, res) => {
-//   if (cache) return res.json(cache);
-//
-//   let page = await getVideos();
-//   let videos = page.items;
-//
-//   while (page.nextPageToken) {
-//     page = await getVideos(page.nextPageToken);
-//     videos = videos.concat(page.items);
-//   }
-//
-//   cache = videos;
-//   res.json(videos);
-// });
-
-//**************working first call
-// app.get("/videos", (req, res) => {
-//   fetch(`${userNameUrl}${youtuber}&key=${process.env.GOOGLE_API_KEY}`)
-//     .then(x => x.json())
-//     .then(json => {
-//       const channelUrl = json.items[0].id;
-//       res.json(json.items[0].id);
-//     });
-// });
+//API 3
+const videoListUrl =
+  "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=";
 
 app.get("/videos", (req, res) => {
   var result = fetch(
@@ -65,36 +38,32 @@ app.get("/videos", (req, res) => {
     })
     .then(function(response) {
       return response.json();
+    })
+    .then(function(data) {
+      var playListId = data.items[0].contentDetails.relatedPlaylists.uploads;
+      console.log(
+        "this is playListId ",
+        data.items[0].contentDetails.relatedPlaylists.uploads
+      );
+      return fetch(
+        `${videoListUrl}${playListId}&key=${process.env.GOOGLE_API_KEY}`
+      );
+    })
+    .then(function(response) {
+      // console.log(response.json());
+      return response.json();
+    })
+    .then(function(data) {
+      var videoId = []; //array of videoId
+      for (var i = 0; i < 5; i++) {
+        videoId.push(data.items[i].snippet.resourceId.videoId);
+      }
+      console.log(videoId);
+    })
+    .catch(function(error) {
+      console.log("request failed", error);
     });
-  // .catch(function(error) {
-  //   console.log("request failed", error);
-  // });
-  result.then(function(data) {
-    var playListId = data.items[0].contentDetails.relatedPlaylists.uploads;
-    console.log(
-      "this is playListId ",
-      data.items[0].contentDetails.relatedPlaylists.uploads
-    );
-  });
 });
-
-// *******************promise attempt
-// app.get("/videos", (req, res) => {
-//   Promise.all([
-//     fetch(`${userNameUrl}${youtuber}&key=${process.env.GOOGLE_API_KEY}`).then(
-//       x => x.json()
-//     ),
-//     fetch(`${userNameUrl}${youtuber}&key=${process.env.GOOGLE_API_KEY}`).then(
-//       x => x.json()
-//     )
-//   ])
-//     .then(x => {
-//       console.log(x);
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// });
 
 function notFound(req, res, next) {
   res.status(404);
