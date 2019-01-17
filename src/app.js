e4abf650596d8793ad02a18e10865a7cb0cfe839
aterna01 <ssaniok90@mail.ru>
2019-01-17 14:27:34 +0000

    created app.js and helpers folder

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
+
+const helpers = require('./views/helpers/index.js');
+const routes = require('./routes/index.js');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set('view engine', 'hbs');
+app.engine(
+    'hbs',
+    handlebars({
+        extname: 'hbs',
+        layoutsDir: path.join(__dirname, 'views', 'layout'),
+        partialsDir: path.join(__dirname, 'views', 'partials'),
+        defaultLayout: 'main'
+    })
+);
+
+app.set('port', process.env.PORT || 5000);
+app.use(routes);
+
+module.exports = app;
