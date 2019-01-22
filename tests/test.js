461d4aa1e508db852eb899fdf4cb45c431c7e0d9
aterna01 <ssaniok90@mail.ru>
2019-01-22 18:00:24 +0000

    zzz

--- tests/test.js
+++ tests/test.js
@@ -2,8 +2,6 @@
 // const func = require('../src/views/helpers/getData.js');
 const sum = require('./test-sum.js');
 const helpers = require('../src/views/helpers/index.js');
-const request = require('supertest');
-const router = require('../src/routes/index.js');
 
 // JEST test on basic fucntion is working
 test('adds 1+2 to equal 3', () => {
@@ -16,20 +14,13 @@ test('check if youtube channel Api result is object', () => {
 });
 
 // test YoutubeApi call is working ( 2 of 4)
+
 const channelCall = helpers.apiYoutube.channel('metallica');
 
 // channelCall.then(x => console.log(x));
 // console.log(channelCall);
 test('check if youtube playlist Api result is object', () => {
     expect(typeof channelCall.then(x => x)).toBe('object');
-});
-
-describe('Test the root path', () => {
-    test('It should response the GET method', () => {
-        request(router)
-            .get('/')
-            .expect(200);
-    });
 });
 
 // test YoutubeApi call is working ( 3 of 4)
