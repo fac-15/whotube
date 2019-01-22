7974656f9ce6139d61f9ed421b09479eea1449da
mike <boimik87@gmail.com>
2019-01-22 15:58:26 +0000

    date and time of tweets alongside text on browser

--- src/views/helpers/handleTweets.js
+++ src/views/helpers/handleTweets.js
@@ -10,7 +10,7 @@ const client = new Twitter({
 
 const handleTweets = search =>
     new Promise((resolve, reject) => {
-        const tweetsArr = [];
+        const tweetsText = [];
         const tweetsTime = [];
         const finalTweetsArr = {};
 
@@ -19,12 +19,12 @@ const handleTweets = search =>
             if (error) {
                 console.log('tweet error', error);
             } else {
-                tweets.forEach(item => tweetsArr.push(item.text));
+                tweets.forEach(item => tweetsText.push(item.text));
                 tweets.forEach(item => tweetsTime.push(item.created_at));
                 // console.log(tweets);
 
                 for (i = 0; i < tweetsTime.length; i++) {
-                    finalTweetsArr[tweetsTime[i]] = tweetsArr[i];
+                    finalTweetsArr[tweetsTime[i]] = tweetsText[i];
                 }
 
                 // (finalTweetsArr = {}),
