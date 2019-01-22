da1d89e19a4705e8a86eb043a0389494303561ca
mike <boimik87@gmail.com>
2019-01-22 14:44:06 +0000

    Revert "set date and time alongside tweets"
    This reverts commit ea4f77044e7c73b503bb35953ea7a49f9623de89.

--- src/views/helpers/handleTweets.js
+++ src/views/helpers/handleTweets.js
@@ -8,38 +8,21 @@ const client = new Twitter({
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-const handleTweets = search =>
+const handleTweets = search => (
     new Promise((resolve, reject) => {
         const tweetsArr = [];
-        const tweetsTime = [];
-        const finalTweetsArr = {};
-
         const params = { screen_name: search, count: 10 };
         client.get('statuses/user_timeline', params, (error, tweets) => {
             if (error) {
                 console.log('tweet error', error);
             } else {
                 tweets.forEach(item => tweetsArr.push(item.text));
-                tweets.forEach(item => tweetsTime.push(item.created_at));
-                // console.log(tweets);
-
-                for (i = 0; i < tweetsTime.length; i++) {
-                    finalTweetsArr[tweetsTime[i]] = tweetsArr[i];
-                }
-
-                // (finalTweetsArr = {}),
-                //     (keys = ['one', 'two', 'three']),
-                //     (values = ['a', 'b', 'c']);
-
-                // for (i = 0; i < keys.length; i++) {
-                //     r[keys[i]] = values[i];
-                // }
-                // console.log('tweetsArr:', tweetsArr);
+                // console.log("tweetsArr:",tweetsArr)
                 // return tweetsArr;
-                console.log('THIS IS THE FINAL OBJ', finalTweetsArr);
-                resolve(finalTweetsArr);
+                resolve(tweetsArr);
             }
         });
-    });
+    })
+);
 
 module.exports = handleTweets;
