2476246ae1ced8bedc0bd8d0fde8ebaaf3eaa454
mike <boimik87@gmail.com>
2019-01-23 17:07:34 +0000

    button is there, but does nothing

---
+++ src/views/helpers/createBookmark.js
@@ -0,0 +1,12 @@
+// const onCreated = node => {
+//     console.log(node);
+// };
+
+// const createBookmark = browser.bookmarks.create({
+//     title: 'bookmarks.create() on MDN',
+//     url: window.location.href
+// });
+
+// createBookmark.then(onCreated);
+
+// module.exports = createBookmark;
--- src/views/helpers/index.js
+++ src/views/helpers/index.js
@@ -2,4 +2,5 @@ module.exports = {
     // getData: require('./getData')
     apiYoutube: require('./youtubeApi'),
     apiTweets: require('./handleTweets')
+    // createBookmark: require('./createBookmark')
 };
